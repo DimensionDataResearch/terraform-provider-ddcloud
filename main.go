c112c8dbb2b6e336b486ab534d51147bb9832707
Samuel Chong <samuel.chong@global.ntt>
2020-11-10 13:13:56 +1100

    fix github build

--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"path"
-
 	"github.com/DimensionDataResearch/dd-cloud-compute-terraform/ddcloud"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 )
 
 // The main program entry-point.
 func main() {
-	if len(os.Args) == 2 && os.Args[1] == "--version" {
-		fmt.Printf("%s %s\n\n", path.Base(os.Args[0]), ddcloud.ProviderVersion)
-
-		return
-	}
+	//if len(os.Args) == 2 && os.Args[1] == "--version" {
+	//	fmt.Printf("%s %s\n\n", path.Base(os.Args[0]), ddcloud.ProviderVersion)
+	//
+	//	return
+	//}
 
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: ddcloud.Provider,
